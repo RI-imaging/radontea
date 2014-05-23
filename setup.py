10596a27cece90bc41b41af550fbc245d400b823
Paul Müller <paul.mueller@biotec.tu-dresden.de>
2014-05-23 14:45:40 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -19,6 +19,20 @@ setup(
     description='Collection of algorithms to compute the inverse Radon transform',
     long_description=open(join(dirname(__file__), 'README.txt')).read(),
     install_requires=[ "NumPy >= 1.5.1",
-                       "SciPy >= 0.8.0"  ]
+                       "SciPy >= 0.8.0"  ],
+    keywords=["tomography", "ct", "radon"],
+    extras_require={
+                    'doc': ['sphinx']
+                   },
+    classifiers= [
+        'Operating System :: Unix',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Scientific/Engineering :: Visualization',
+        'Intended Audience :: Science/Research'
+                 ],
+    platforms=['ALL']
     )
 
