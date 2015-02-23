7a9e1a06678548d823908daab8c22508bb29c6c5
Paul Müller <paul.mueller@biotec.tu-dresden.de>
2015-02-23 12:35:05 +0100

    untested automated doc upload

--- doc/make.py
+++ doc/make.py
@@ -4,4 +4,8 @@ import subprocess as sp
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+# remove old html files
+sp.check_output(["rm", '-r _build/html'])
+
+# compile docs
 sp.check_output(["make", 'html'])
--- doc/upload.py
+++ doc/upload.py
@@ -7,8 +7,19 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 # compile
 sp.check_output([sys.executable, 'make.py'])
 
+os.chdir("..")
+
 # checkout the gh-pages branch
 sp.check_output(["git", 'checkout gh-pages'])
 
 # copy built files
-sp.check_output(["git", 'commit -a -m "automated "'])
+sp.check_output(["cp", '-r ./doc/_build/html/* ./'])
+
+# commit changes
+sp.check_output(["git", 'commit -a -m "automated doc upload"'])
+
+# push
+sp.check_output(["git", 'push'])
+
+# go back to master
+sp.check_output(["git", 'checkout master'])
