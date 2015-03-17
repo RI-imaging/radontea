5b2e39b279f7e6ce1bd77e18752dbe894989c425
Paul Müller <paul.mueller@biotec.tu-dresden.de>
2015-03-17 17:46:59 +0100

    changes before doc commit

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 # To create a distribution package for pip or easy-install:
 # python setup.py sdist
 from setuptools import setup, find_packages, Command
