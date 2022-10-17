58589559a573119afbf575a9e39734d83145674a
Paul Müller <dev@craban.de>
2022-10-17 17:39:36 +0200

    ref: make util submodule available upon import

--- radontea/__init__.py
+++ radontea/__init__.py
@@ -1,15 +1,17 @@
-from ._alg_bpj import backproject, backproject_3d  # noqa F401
-from ._alg_fmp import fourier_map, fourier_map_3d  # noqa F401
-from ._alg_int import integrate  # noqa F401
-from ._alg_art import art  # noqa F401
-from ._alg_sart import sart  # noqa F401
+# flake8: noqa F401
+from ._alg_bpj import backproject, backproject_3d
+from ._alg_fmp import fourier_map, fourier_map_3d
+from ._alg_int import integrate
+from ._alg_art import art
+from ._alg_sart import sart
 
-from ._rdn_prl import radon_parallel  # noqa F401
-from ._threed import volume_recon  # noqa F401
+from ._rdn_prl import radon_parallel
+from ._threed import volume_recon
 
-from ._version import version as __version__  # noqa F401
+from ._version import version as __version__
 
-from . import fan  # noqa F401
+from . import fan
+from . import util
 
 
 __author__ = "Paul Müller"
